cafc692bcea1066cd8a81b1e674b5c6767daddbf
n3rd-bugs <mirza.usama@yahoo.com>
2014-08-17 15:08:08 +0500

    Added some comments in os.h, os.c, os_avr.c. Defined and added a copyright notice.

--- avr/os_avr.c
+++ avr/os_avr.c
@@ -1,8 +1,14 @@
 /*
+ * os_avr.c
+ *
  * Copyright (c) 2014 Usama Masood <mirzaon@gmail.com>
  *
+ * Standard MIT License apply on this source code, with the inclusion of below
+ * clause.
+ *
  * This source is for educational purpose only, and should never be used for
- * any other purpose.
+ * any other purpose. If this source is used for other than educational purpose
+ * (in any form) the author will not be liable for any legal charges.
  */
 #include <avr/interrupt.h>
 #include <os.h>
--- src/os.c
+++ src/os.c
@@ -1,8 +1,14 @@
 /*
+ * os.c
+ *
  * Copyright (c) 2014 Usama Masood <mirzaon@gmail.com>
  *
+ * Standard MIT License apply on this source code, with the inclusion of below
+ * clause.
+ *
  * This source is for educational purpose only, and should never be used for
- * any other purpose.
+ * any other purpose. If this source is used for other than educational purpose
+ * (in any form) the author will not be liable for any legal charges.
  */
 #include <os.h>
 #include <os_avr.h>
