4f37cb5c99597d3bce8fa2a1470e69d438bb4f6f
N3rd Bugs <mirzaon@gmail.com>
2015-01-03 15:35:46 +0500

    Updating memory initializer.

--- rtos/mem/mem.h
+++ rtos/mem/mem.h
@@ -45,12 +45,12 @@ void mem_init();
 
 #ifdef CONFIG_MEMGR_STATIC
 #define mem_static_alloc(size)      mem_static_alloc_region(&mem_static, size)
-#define mem_static_dealloc(mem)     mem_static_dealloc_region(mem)
+#define mem_static_dealloc(mem)     mem_static_dealloc_region((char *)mem)
 #endif
 
 #ifdef CONFIG_MEMGR_DYNAMIC
 #define mem_dynamic_alloc(size)     mem_dynamic_alloc_region(&mem_dynamic, size)
-#define mem_dynamic_dealloc(mem)    mem_dynamic_dealloc_region(mem)
+#define mem_dynamic_dealloc(mem)    mem_dynamic_dealloc_region((char *)mem)
 #endif
 
 #endif
