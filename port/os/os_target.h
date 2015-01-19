bb86b0a5a0bae3115f89f59f5ce1b5060a006ebc
N3rd Bugs <mirzaon@gmail.com>
2015-01-20 00:56:54 +0500

    Updated target configuration.

--- port/os/os_target.h
+++ port/os/os_target.h
@@ -13,21 +13,47 @@
 #ifndef OS_TARGET_H
 #define OS_TARGET_H
 
+/* Target processor definitions. */
 #define TARGET_AVR          0x1
 #define TARGET_CORTEX_M3    0x2
+#define TARGET_CORTEX_M4    0x3
 
-#define RTOS_TARGET         TARGET_AVR
+/* Target platform definitions. */
+#define PLAT_PK40X256VLQ100 0x01
+#define PLAT_STM32F407_DISC 0x02
 
-/* Target includes. */
+/* Target toolset configuration. */
+#define TOOL_ARM_GCC        0x01
+
+/* RTOS configuration. */
+#define PLAT_TARGET         PLAT_PK40X256VLQ100
+#define RTOS_TARGET         TARGET_CORTEX_M4
+#define TOOL_TARGET         TOOL_ARM_GCC
+
+/* Toolset includes. */
+#if (TOOL_TARGET == TOOL_ARM_GCC)
+#include <os_arm_gcc.h>
+#endif
+#include <tasks.h>
+
+/* Processor includes. */
 #if (RTOS_TARGET == TARGET_AVR)
 #include <os_avr.h>
 #endif
 #if (RTOS_TARGET == TARGET_CORTEX_M3)
 #include <os_cortex_m3.h>
-#include <MK40DZ10.h>
+#endif
+#if (RTOS_TARGET == TARGET_CORTEX_M4)
+#include <os_cortex_m4.h>
 #endif
 
-#include <tasks.h>
+/* Platform includes. */
+#if (PLAT_TARGET == PLAT_STM32F407_DISCOVERY)
+#include <os_stm32f407.h>
+#endif
+#if (PLAT_TARGET == PLAT_PK40X256VLQ100)
+#include <os_pk40x256vlq100.h>
+#endif
 
 /* Following functions must be implemented by target porting layer. */
 void system_tick_Init();
