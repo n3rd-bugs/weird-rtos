a48f22c8dbd4e7ed3f36aa691c6e21dc8033fe90
N3rd Bugs <mirzaon@gmail.com>
2015-01-20 01:02:11 +0500

    Adding port for Cortex-M4.

---
+++ port/os/cortex-m4/os_cortex_m4.c
@@ -0,0 +1,238 @@
+/*
+ * os_cortex_m4.c
+ *
+ * Copyright (c) 2015 Usama Masood <mirzaon@gmail.com>
+ *
+ * Standard MIT License apply on this source code, with the inclusion of below
+ * clause.
+ *
+ * This source is for educational purpose only, and should never be used for
+ * any other purpose. If this source is used for other than educational purpose
+ * (in any form) the author will not be liable for any legal charges.
+ */
+
+#include <os.h>
+
+/* Global interrupt level. */
+/* TRUE: Interrupt Enabled
+ * FALSE: Interrupt Disabled */
+uint32_t sys_interrupt_level = TRUE;
+
+/* Local variable definitions. */
+/* We need to keep this to save the stack pointer for the task we are
+ * switching from. */
+/* Removing this requires implementation of naked ISR functions. */
+static TASK *last_task;
+
+/*
+ * os_stack_init
+ * @tcb: Task control block needed to be initialized.
+ * @task_entry: Task entry function.
+ * @argv: Arguments to be passed to the entry function.
+ * This function is responsible for initializing stack for a tack.
+ */
+void os_stack_init(TASK *tcb, TASK_ENTRY *task_entry, void *argv)
+{
+    hardware_stack_farme *init_stack_frame;
+    software_stack_farme *init_soft_stack_frame;
+
+    /* Push a hardware stack frame on the stack. */
+    tcb->tos -= sizeof(hardware_stack_farme);
+
+    /* Initialize stack frame, this will be loaded when CPU
+     * the will jump in the task first time. */
+    init_stack_frame        = (hardware_stack_farme *)(tcb->tos);
+    init_stack_frame->r0    = (uint32_t)argv;
+    init_stack_frame->r1    = 0;
+    init_stack_frame->r2    = 0;
+    init_stack_frame->r3    = 0;
+    init_stack_frame->r12   = 0;
+    init_stack_frame->pc    = (uint32_t)task_entry;
+    init_stack_frame->lr    = (uint32_t)0x0;
+    init_stack_frame->psr   = INITIAL_XPSR;
+
+    /* Push a dummy software stack frame. */
+    tcb->tos -= sizeof(software_stack_farme);
+
+    init_soft_stack_frame = (software_stack_farme *)(tcb->tos);
+    init_soft_stack_frame->r14 = 0xFFFFFFFD;
+
+#ifdef CONFIG_TASK_STATS
+    /* Break the task stack pattern. */
+    *(tcb->tos - 1) = 0x00;
+#endif /* CONFIG_TASK_STATS */
+
+} /* os_stack_init */
+
+/*
+ * run_first_task
+ * This is responsible for running first task.
+ */
+void run_first_task()
+{
+    /* Disable interrupts. */
+    DISABLE_INTERRUPTS();
+
+    /* We are running the first task so there is no last task. */
+    last_task = NULL;
+
+    /* Set default interrupt level as disabled. */
+    sys_interrupt_level = FALSE;
+
+    /* Set PendSV and System Tick interrupt priorities to avoid nested
+     * interrupts. */
+    CORTEX_M4_INT_PEND_SV_PRI_REG = CORTEX_M4_INT_SYS_PRI;
+    CORTEX_M4_INT_SYS_TICK_PRI_REG = CORTEX_M4_INT_SYS_PRI;
+
+    /* Schedule a context switch. */
+    PEND_SV();
+
+    __asm volatile
+    (
+    "   LDR     R0, =0xE000ED08   \n"
+    "   LDR     R0, [R0]          \n"
+    "   LDR     R0, [R0]          \n"
+    "   MSR     MSP, R0           \n"
+    );
+
+    /* Enable interrupts. */
+    ENABLE_INTERRUPTS();
+
+} /* run_first_task */
+
+/*
+ * control_to_system
+ */
+void control_to_system()
+{
+    /* Save the task from which we will be switching. */
+    last_task = current_task;
+
+    /* Process and get the next task in this task's context. */
+    current_task = scheduler_get_next_task();
+
+    /* Check if we need to switch context. */
+    if (current_task != last_task)
+    {
+        /* Schedule a context switch. */
+        PEND_SV();
+    }
+
+    /* Enable interrupts. */
+    ENABLE_INTERRUPTS();
+
+} /* run_first_task */
+
+/*
+ * isr_sysclock_handle
+ * This is system tick interrupt handle.
+ */
+ISR_FUN isr_sysclock_handle(void)
+{
+    /* Disable interrupts. */
+    DISABLE_INTERRUPTS();
+
+    /* Save the current task pointer. */
+    last_task = current_task;
+
+    /* Process system tick. */
+    os_process_system_tick();
+
+    /* Check if we need to switch context. */
+    if (current_task != last_task)
+    {
+        /* Schedule a context switch. */
+        PEND_SV();
+    }
+
+    /* Enable interrupts. */
+    ENABLE_INTERRUPTS();
+
+} /* isr_sysclock_handle */
+
+/*
+ * isr_pendsv_handle
+ * This pendSV interrupt handle.
+ */
+NAKED_ISR_FUN isr_pendsv_handle(void)
+{
+    /* Disable global interrupts. */
+    DISABLE_INTERRUPTS();
+
+    /* Check if need to save last task context. */
+    if (last_task)
+    {
+        /* Save last task context on stack. */
+        asm volatile
+        (
+        "   MRS         %[sp], PSP              \r\n"
+        "   STMDB       %[sp]!, {R4 - R11, R14} \r\n"
+#if (CORTEX_M4_FPU == TRUE)
+        "   TST         R14, #0x10              \r\n"
+        "   IT          eq                      \r\n"
+        "   VSTMDBEQ    %[sp]!, {S16 - S31}     \r\n"
+#endif
+        :
+        [sp] "=r" (last_task->tos)
+        );
+
+#ifdef CONFIG_TASK_STATS
+        /* Break the task stack pattern. */
+        *(last_task->tos - 1) = 0x00;
+#endif /* CONFIG_TASK_STATS */
+    }
+
+    /* Load context for new task. */
+    asm volatile
+    (
+    "   LDMIA       %[sp]!, {R4 - R11, R14} \r\n"
+#if (CORTEX_M4_FPU == TRUE)
+    "   TST         R14, #0x10              \r\n"
+    "   IT          eq                      \r\n"
+    "   VLDMIAEQ    %[sp]!, {S16 - S31}     \r\n"
+#endif
+    "   MSR         PSP, %[sp]              \r\n"
+    ::
+    [sp] "r" (current_task->tos)
+    );
+
+    /* Just enable system tick interrupt. */
+    CORTEX_M4_SYS_TICK_REG |= (CORTEX_M4_SYS_TICK_MASK);
+
+    /* Enable interrupts and return from this function. */
+    RETURN_ENABLING_INTERRUPTS();
+
+} /* isr_pendsv_handle */
+
+/*
+ * cpu_interrupt
+ * Default ISR callback.
+ */
+ISR_FUN cpu_interrupt(void)
+{
+    /* Assert the system. */
+    OS_ASSERT(TRUE);
+
+} /* cpu_interrupt */
+
+/*
+ * nmi_interrupt
+ * NMI interrupt callback.
+ */
+ISR_FUN nmi_interrupt(void)
+{
+    /* Assert the system. */
+    OS_ASSERT(TRUE);
+
+} /* nmi_interrupt */
+
+/*
+ * hard_fault_interrupt
+ * Hard fault interrupt callback.
+ */
+ISR_FUN hard_fault_interrupt(void)
+{
+    /* Assert the system. */
+    OS_ASSERT(TRUE);
+
+} /* hard_fault_interrupt */
---
+++ port/os/cortex-m4/os_cortex_m4.h
@@ -0,0 +1,118 @@
+/*
+ * os_cortex_m3.h
+ *
+ * Copyright (c) 2014 Usama Masood <mirzaon@gmail.com>
+ *
+ * Standard MIT License apply on this source code, with the inclusion of below
+ * clause.
+ *
+ * This source is for educational purpose only, and should never be used for
+ * any other purpose. If this source is used for other than educational purpose
+ * (in any form) the author will not be liable for any legal charges.
+ */
+#ifndef OS_CORTEX_M4_H
+#define OS_CORTEX_M4_H
+
+#include <os.h>
+
+/* Peripheral clock configuration. */
+#define SYS_FREQ                168000000
+#define PCLK_FREQ               24000000
+
+/* System interrupt level. */
+extern uint32_t sys_interrupt_level;
+
+/* Macros to manipulate interrupts. */
+#define DISABLE_INTERRUPTS()            {                               \
+                                            asm("   CPSID   I   ");     \
+                                            sys_interrupt_level = 0;    \
+                                        }
+
+#define ENABLE_INTERRUPTS()             {                               \
+                                            sys_interrupt_level = 1;    \
+                                            asm("   DSB         ");     \
+                                            asm("   ISB         ");     \
+                                            asm("   CPSIE   I   ");     \
+                                        }
+#define GET_INTERRUPT_LEVEL()           (sys_interrupt_level)
+#define SET_INTERRUPT_LEVEL(n)          {                               \
+                                            if (n == 0)                 \
+                                            {                           \
+                                                DISABLE_INTERRUPTS();   \
+                                            }                           \
+                                            else                        \
+                                            {                           \
+                                                ENABLE_INTERRUPTS();    \
+                                            }                           \
+                                        }
+
+#define RESTORE_CONTEXT_FIRST()         run_first_task()
+#define PEND_SV()                       {                                           \
+                                            asm("   DSB    ");                      \
+                                            asm("   ISB    ");                      \
+                                            CORTEX_M4_PEND_SV_REG |= CORTEX_M4_PEND_SV_MAST;     \
+                                            CORTEX_M4_SYS_TICK_REG &= ~(CORTEX_M4_SYS_TICK_MASK);\
+                                        }
+
+#define CONTROL_TO_SYSTEM()             control_to_system()
+
+#define RETURN_ENABLING_INTERRUPTS()    {                               \
+                                            ENABLE_INTERRUPTS();        \
+                                            asm("   BX      LR  ");     \
+                                        }
+
+#define INITIAL_XPSR                    0x01000000
+
+/* Memory definitions. */
+#define STATIC_MEM_START                ((char *)(&static_start))
+#define STATIC_MEM_END                  ((char *)(&static_end))
+#define DYNAMIC_MEM_START               ((char *)(&dynamic_start))
+#define DYNAMIC_MEM_END                 ((char *)(&dynamic_end))
+
+typedef struct _hardware_stack_farme
+{
+    uint32_t r0;
+    uint32_t r1;
+    uint32_t r2;
+    uint32_t r3;
+    uint32_t r12;
+    uint32_t lr;
+    uint32_t pc;
+    uint32_t psr;
+} hardware_stack_farme;
+
+typedef struct _software_stack_farme
+{
+    uint32_t r4;
+    uint32_t r5;
+    uint32_t r6;
+    uint32_t r7;
+    uint32_t r8;
+    uint32_t r9;
+    uint32_t r10;
+    uint32_t r11;
+    uint32_t r14;
+} software_stack_farme;
+
+#define TOS_SET(tos, sp, size)      (tos = (sp + size))
+
+/* Exported variables. */
+extern uint32_t static_start;
+extern uint32_t static_end;
+extern uint32_t dynamic_start;
+extern uint32_t dynamic_end;
+
+/* Function prototypes. */
+void run_first_task();
+void control_to_system();
+
+/* System interrupt definitions. */
+ISR_FUN cpu_interrupt(void);
+ISR_FUN nmi_interrupt(void);
+ISR_FUN hard_fault_interrupt(void);
+ISR_FUN isr_servicecall_handle(void);
+NAKED_ISR_FUN isr_pendsv_handle(void);
+ISR_FUN isr_sysclock_handle(void);
+ISR_FUN isr_clock64_tick(void);
+
+#endif /* OS_CORTEX_M4_H */
