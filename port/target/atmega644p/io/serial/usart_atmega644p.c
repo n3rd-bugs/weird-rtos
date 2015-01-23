8d3202302d21f86465a4cce27cf551abb89599c7
N3rd Bugs <mirzaon@gmail.com>
2015-01-24 00:23:33 +0500

    Updating console for AVR.

---
+++ port/target/atmega644p/io/serial/usart_atmega644p.c
@@ -0,0 +1,135 @@
+/*
+ * usart_atmega644p.c
+ *
+ * Copyright (c) 2014 Usama Masood <mirzaon@gmail.com>
+ *
+ * Standard MIT License apply on this source code, with the inclusion of below
+ * clause.
+ *
+ * This source is for educational purpose only, and should never be used for
+ * any other purpose. If this source is used for other than educational purpose
+ * (in any form) the author will not be liable for any legal charges.
+ */
+#include <os.h>
+#include <avr/io.h>
+#include <stdio.h>
+#define BAUD    BAUD_RATE
+#include <util/setbaud.h>
+
+#ifdef FS_CONSOLE
+/* Debug file descriptor. */
+FD debug_fd;
+
+/* Console data. */
+CONSOLE usart_1 =
+{
+    .fs =
+    {
+        /* Name of this port. */
+        .name = "uart1",
+
+        /* Console manipulation APIs. */
+        .write = &usart_atmega644p_puts,
+    }
+};
+#endif
+
+/*
+ * usart_atmega644p_puts
+ * @priv_data: For now it is unused.
+ * @buf: String needed to be printed.
+ * @nbytes: Number of bytes to be printed from the string.
+ * This function prints a string on the USART.
+ */
+uint32_t usart_atmega644p_puts(void *priv_data, char *buf, uint32_t nbytes)
+{
+    uint32_t to_print = nbytes;
+
+    /* Remove some compiler warnings. */
+    UNUSED_PARAM(priv_data);
+
+    /* While we have some data to be printed. */
+    while(nbytes > 0)
+    {
+        /* Wait for last byte to be sent. */
+        loop_until_bit_is_set(UCSR0A, UDRE0);
+
+        /* Add this byte. */
+        UDR0 = (*buf);
+
+        /* Decrement number of bytes remaining. */
+        nbytes --;
+
+        /* Move forward in the buffer. */
+        buf++;
+    }
+
+    /* Return number of bytes printed. */
+    return (to_print - nbytes);
+
+} /* usart_atmega644p_puts */
+
+/*
+ * uart_atmega644p_printf
+ * @format: Formated string to be printed on USART.
+ * This function prints a formated string on the USART.
+ */
+uint32_t uart_atmega644p_printf(char *format, ...)
+{
+    uint32_t n = 0;
+    char buf[100];
+    va_list vl;
+
+    /* Arguments start from the format. */
+    va_start(vl, format);
+
+    /* Process the given string and save the result in a temporary buffer. */
+    n = vsnprintf(buf, 100, format, vl);
+
+#ifdef FS_CONSOLE
+    /* Use the debug FD. */
+    n = fs_write(debug_fd, buf, n);
+#else
+    /* Print the result on the UART. */
+    n = usart_atmega644p_puts(NULL, buf, n);
+#endif
+
+    /* Destroy the argument list. */
+    va_end(vl);
+
+    /* Return number of bytes printed on UART. */
+    return (n);
+
+} /* uart_atmega644p_printf */
+
+/*
+ * usart_atmega644p_init
+ * This function initializes the AVR serial console over USART.
+ */
+void usart_atmega644p_init()
+{
+    /* Set the configured baud-rate. */
+    UBRR0H = UBRRH_VALUE;
+    UBRR0L = UBRRL_VALUE;
+
+#if USE_2X
+    UCSRA |= (1 << U2X0);
+#else
+    UCSR0A &= ~(1 << U2X0);
+#endif
+
+    /* Enable RX and TX. */
+    UCSR0B = (1 << RXEN0)  | (1 << TXEN0);
+    UCSR0C = (1 << USBS0) | (3 << UCSZ00);
+
+#ifdef FS_CONSOLE
+
+    /* Register serial port with console. */
+    console_register(&usart_1);
+
+    /* Set debug file descriptor. */
+    debug_fd = fs_open("\\console\\usart1", 0);
+
+#endif /* FS_CONSOLE */
+
+} /* usart_atmega644p_init */
---
+++ port/target/atmega644p/io/serial/usart_atmega644p.h
@@ -0,0 +1,35 @@
+/*
+ * usart_atmega644p.h
+ *
+ * Copyright (c) 2014 Usama Masood <mirzaon@gmail.com>
+ *
+ * Standard MIT License apply on this source code, with the inclusion of below
+ * clause.
+ *
+ * This source is for educational purpose only, and should never be used for
+ * any other purpose. If this source is used for other than educational purpose
+ * (in any form) the author will not be liable for any legal charges.
+ */
+#ifndef _USART_ATMEGA664P_H_
+#define _USART_ATMEGA664P_H_
+
+#include <stdio.h>
+
+/* Hook-up the STDIO printf function. */
+#ifdef printf
+#undef printf
+#endif
+#define printf uart_atmega644p_printf
+
+/* Used by console file system to initialize DEBUG console. */
+#define DEBUG_CONSOLE_INIT  usart_atmega644p_init
+
+/* Some configurations. */
+#define BAUD_RATE       115200
+
+/* Function prototypes. */
+uint32_t usart_atmega644p_puts(void *priv_data, char *buf, uint32_t nbytes);
+uint32_t uart_atmega644p_printf(char *format, ...);
+void usart_atmega644p_init();
+
+#endif /* _USART_ATMEGA664P_H_ */
