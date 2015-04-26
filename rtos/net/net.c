15cc54079b6ae78f9220f969c8dfd06114c20e35
N3rd Bugs <mirzaon@gmail.com>
2015-04-27 00:02:57 +0500

    Added socket structure and it's manipulation routines.

--- rtos/net/net.c
+++ rtos/net/net.c
@@ -15,6 +15,9 @@
 #ifdef CONFIG_NET
 #include <net.h>
 #include <net_condition.h>
+#ifdef NET_UDP
+#include <net_udp.h>
+#endif
 
 /*
  * net_init
@@ -31,6 +34,86 @@ void net_init()
     /* Initialize networking conditions. */
     net_condition_init();
 
+#ifdef NET_UDP
+    /* Initialize UDP stack. */
+    udp_initialize();
+#endif
+
 } /* net_buffer_init */
+
+/*
+ * net_port_match
+ * @port1: Port needed to be matched.
+ * @port2: Given port.
+ * @return: A true will be returned if ports match exactly, partial will be
+ *  returned if they match partially and false will be returned if ports don't
+ *  match at all.
+ * This function will match two given networking ports.
+ */
+uint8_t net_port_match(uint16_t port1, uint16_t port2, uint8_t match)
+{
+    /* If match is not already failed. */
+    if (match != FALSE)
+    {
+        /* Compare the two networking ports. */
+
+        /* If we don't have an unspecified port. */
+        if (port1 != NET_PORT_UNSPEC)
+        {
+            /* If the address exactly match. */
+            if (port1 == port2)
+            {
+                /* A partial match cannot be updated to exact match. */
+                if (match != PARTIAL)
+                {
+                    /* Exact match. */
+                    match = TRUE;
+                }
+            }
+            else
+            {
+                /* Did not match at all. */
+                match = FALSE;
+            }
+        }
+        else
+        {
+            /* Got a partial match. */
+            match = PARTIAL;
+        }
+    }
+
+    /* Return the match status to the caller. */
+    return (match);
+
+} /* net_port_match */
+
+/*
+ * net_socket_match
+ * @socket1: Socket needed to be matched.
+ * @socket2: Given socket address.
+ * @return: A true will be returned if sockets match exactly, partial will be
+ *  returned if they match partially and false will be returned if sockets don't
+ *  match at all.
+ * This function will match two given networking sockets.
+ */
+uint8_t net_socket_match(SOCKET *socket1, SOCKET *socket2)
+{
+    uint8_t match;
+
+#ifdef NET_IPV4
+    /* Compare the local and foreign IP addresses. */
+    match = ipv4_compare_address(socket1->local_ip, socket2->local_ip, TRUE);
+    match = ipv4_compare_address(socket1->foreign_ip, socket2->foreign_ip, match);
+#endif
+
+    /* Compare the local and foreign ports. */
+    match = net_port_match(socket1->local_port, socket2->local_port, TRUE);
+    match = net_port_match(socket1->foreign_port, socket2->foreign_port, match);
+
+    /* Return the match status to the caller. */
+    return (match);
+
+} /* net_socket_match */
 
 #endif /* CONFIG_NET */
--- rtos/net/net.h
+++ rtos/net/net.h
@@ -29,14 +29,34 @@
 #define NET_BUFFER_CONSUMED     -1000
 #define NET_NOT_SUPPORTED       -1001
 #define NET_UNKNOWN_PROTO       -1002
-#define NET_DEST_UNREACHABLE    -1003
-#define NET_NO_BUFFERS          -1004
-#define NET_INVALID_CSUM        -1005
-#define NET_INVALID_HDR         -1006
-#define NET_INVALID_FD          -1007
+#define NET_DST_UNREACHABLE     -1003
+#define NET_DST_PRT_UNREACHABLE -1004
+#define NET_NO_BUFFERS          -1005
+#define NET_INVALID_CSUM        -1006
+#define NET_INVALID_HDR         -1007
+#define NET_INVALID_FD          -1008
+
+/* Networking port definitions. */
+#define NET_PORT_UNSPEC         0
+
+/* Socket structure. */
+typedef struct _socket
+{
+#ifdef NET_IPV4
+    /* IPv4 data for this socket. */
+    uint32_t    local_ip;
+    uint32_t    foreign_ip;
+#endif
+
+    /* Port data for this socket. */
+    uint16_t    local_port;
+    uint16_t    foreign_port;
+} SOCKET;
 
 /* Function prototypes. */
 void net_init();
+uint8_t net_port_match(uint16_t, uint16_t, uint8_t);
+uint8_t net_socket_match(SOCKET *, SOCKET *);
 
 #endif /* CONFIG_NET */
 #endif /* _NET_H_ */
