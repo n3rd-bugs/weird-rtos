7846d52127684487e80cc06cfc4a71a0716e94b3
N3rd Bugs <mirzaon@gmail.com>
2015-05-13 00:44:39 +0500

    Added PHY read and control word read APIs and separated PHY wait logic.

--- rtos/io/etherent/enc28j60/enc28j60_spi.c
+++ rtos/io/etherent/enc28j60/enc28j60_spi.c
@@ -16,55 +16,187 @@
 #include <enc28j60.h>
 #include <enc28j60_spi.h>
 
+/* Internal function prototypes. */
+static int32_t enc28j60_phy_wait(ENC28J60 *);
+
+/*
+ * enc28j60_phy_wait
+ * @device: ENC28J60 device instance for which we need to wait for PHY
+ *  operation.
+ * @return: A success status will be returned if PHY operation was successfully
+ *  completed, ENC28J60_SPI_ERROR will be returned if an error occurred while
+ *  writing on SPI device.
+ * This function will perform wait for a PHY operation to be completed on the
+ * given enc28j60 device.
+ */
+static int32_t enc28j60_phy_wait(ENC28J60 *device)
+{
+    int32_t status;
+    uint8_t mistat;
+
+    /* Wait for PHY register to be written. */
+    do
+    {
+        /* Takes around 10.24 micro seconds, just sleep for 1 tick that
+         * should be enough. */
+        sleep(1);
+
+        /* Read the value of MISTAT. */
+        status = enc28j60_write_read_op(device, ENC28J60_OP_READ_BUFFER, ENC28J60_ADDR_MISTAT, 0xFF, &mistat);
+
+    } while ((status == SUCCESS) && (mistat & ENC28J60_MISTAT_BUSY));
+
+    /* Return status to the caller. */
+    return (status);
+
+} /* enc28j60_phy_wait */
+
+
 /*
  * enc28j60_write_phy
- * @device: ENC28J60 device instance for PHY register is needed to be written.
+ * @device: ENC28J60 device instance for which PHY register is needed to be
+ *  written.
  * @address: Address at which data is needed to be written.
  * @value: Value needed to be written.
  * @return: A success status will be returned if a register was successfully
  *  written, ENC28J60_SPI_ERROR will be returned if an error occurred while
  *  writing on SPI device.
- * This function will perform write a PHY register on given ENC28J60 device.
+ * This function will perform write a PHY register on given enc28j60 device.
  */
 int32_t enc28j60_write_phy(ENC28J60 *device, uint8_t address, uint16_t value)
 {
     int32_t status;
-    uint8_t mistat;
 
     /* Write the PHY address needed to be written on MIREGADR. */
     status = enc28j60_write_read_op(device, ENC28J60_OP_WRITE_CTRL, ENC28J60_ADDR_MIREGADR, address, NULL);
 
     if (status == SUCCESS)
     {
-        /* Write the data on MIWRL. */
-        status = enc28j60_write_read_op(device, ENC28J60_OP_WRITE_CTRL, ENC28J60_ADDR_MIWRL, (uint8_t)(value & 0xFF), NULL);
+        /* Write the data on MIWRL/MIWRH. */
+        status = enc28j60_write_word(device, ENC28J60_ADDR_MIWRL, value);
     }
 
     if (status == SUCCESS)
     {
-        /* Write the data on MIWRH. */
-        status = enc28j60_write_read_op(device, ENC28J60_OP_WRITE_CTRL, ENC28J60_ADDR_MIWRH, (uint8_t)(value >> 8), NULL);
-    }
-
-    if (status == SUCCESS)
-    {
-        /* Wait for PHY register to be written. */
-        do
-        {
-            /* Takes around 10.24 micro seconds, just sleep for 1 tick that
-             * should be enough. */
-            sleep(1);
-
-            /* Read the value of MISTAT. */
-            status = enc28j60_write_read_op(device, ENC28J60_OP_READ_BUFFER, ENC28J60_ADDR_MISTAT, 0xFF, &mistat);
-
-        } while ((status == SUCCESS) && (mistat & ENC28J60_MISTAT_BUSY));
+        /* Wait for the completion of PHY operation. */
+        status = enc28j60_phy_wait(device);
     }
 
     /* Return status to the caller. */
     return (status);
 
 } /* enc28j60_write_phy */
+
+/*
+ * enc28j60_read_phy
+ * @device: ENC28J60 device instance for PHY register is needed to be read.
+ * @address: Address from which data is needed to be read.
+ * @value: If not null read data will be returned here.
+ * @return: A success status will be returned if a register was successfully
+ *  read, ENC28J60_SPI_ERROR will be returned if an error occurred while
+ *  reading from SPI device.
+ * This function will read a PHY register from given ENC28J60 device.
+ */
+int32_t enc28j60_read_phy(ENC28J60 *device, uint8_t address, uint16_t *value)
+{
+    int32_t status;
+
+    /* Set the PHY address needed to be read at MIREGADR. */
+    status = enc28j60_write_read_op(device, ENC28J60_OP_WRITE_CTRL, ENC28J60_ADDR_MIREGADR, address, NULL);
+
+    if (status == SUCCESS)
+    {
+        /* Start the PHY read operation. */
+        status = enc28j60_write_read_op(device, ENC28J60_OP_WRITE_CTRL, ENC28J60_ADDR_MICMD, ENC28J60_MICMD_MIIRD, NULL);
+    }
+
+    if (status == SUCCESS)
+    {
+        /* Wait for the completion of PHY operation. */
+        status = enc28j60_phy_wait(device);
+    }
+
+    if (status == SUCCESS)
+    {
+        /* Stop the PHY read operation. */
+        status = enc28j60_write_read_op(device, ENC28J60_OP_WRITE_CTRL, ENC28J60_ADDR_MICMD, 0x00, NULL);
+    }
+
+    if (status == SUCCESS)
+    {
+        /* Read and return the PHY register. */
+        status = enc28j60_read_word(device, ENC28J60_ADDR_MIRDL, value);
+    }
+
+    /* Return status to the caller. */
+    return (status);
+
+} /* enc28j60_read_phy */
+
+/*
+ * enc28j60_write_word
+ * @device: ENC28J60 device instance for which data is needed to be written.
+ * @address: Address at which data is needed to be written.
+ * @value: Value needed to be written.
+ * @return: A success status will be returned if a register was successfully
+ *  written, ENC28J60_SPI_ERROR will be returned if an error occurred while
+ *  writing on SPI device.
+ * This function will perform write a word (2-bytes) on the given address of
+ * enc28j60 device.
+ */
+int32_t enc28j60_write_word(ENC28J60 *device, uint8_t address, uint16_t value)
+{
+    int32_t status;
+
+    /* Write the first byte. */
+    status = enc28j60_write_read_op(device, ENC28J60_OP_WRITE_CTRL, (address), (uint8_t)(value & 0xFF), NULL);
+
+    if (status == SUCCESS)
+    {
+        /* Write the second byte. */
+        status = enc28j60_write_read_op(device, ENC28J60_OP_WRITE_CTRL, (uint8_t)(address + 1), (uint8_t)(value >> 8), NULL);
+    }
+
+    /* Return status to the caller. */
+    return (status);
+
+} /* enc28j60_write_word */
+
+/*
+ * enc28j60_read_word
+ * @device: ENC28J60 device instance for which data is needed to be read.
+ * @address: Address from which data is needed to be read.
+ * @value: If not null read value will be returned here.
+ * @return: A success status will be returned if a register was successfully
+ *  read, ENC28J60_SPI_ERROR will be returned if an error occurred while
+ *  reading from SPI device.
+ * This function will read a word (2-bytes) from the given address of
+ * enc28j60 device.
+ */
+int32_t enc28j60_read_word(ENC28J60 *device, uint8_t address, uint16_t *value)
+{
+    int32_t status;
+    uint8_t first_byte, second_byte;
+
+    /* Read the first byte. */
+    status = enc28j60_write_read_op(device, ENC28J60_OP_WRITE_CTRL, (address), 0xFF, &first_byte);
+
+    if (status == SUCCESS)
+    {
+        /* Read the second byte. */
+        status = enc28j60_write_read_op(device, ENC28J60_OP_WRITE_CTRL, (uint8_t)(address + 1), 0xFF, &second_byte);
+    }
+
+    if ((status == SUCCESS) && (value != NULL))
+    {
+        /* Return the read word. */
+        *value = (uint16_t)((second_byte << 8) | first_byte);
+    }
+
+    /* Return status to the caller. */
+    return (status);
+
+} /* enc28j60_read_word */
 
 /*
  * enc28j60_write_read_op
--- rtos/io/etherent/enc28j60/enc28j60_spi.h
+++ rtos/io/etherent/enc28j60/enc28j60_spi.h
@@ -30,34 +30,46 @@
 #define ENC28J60_BANK_1             (ENC28J60_BANKED_MASK | 0x40)
 #define ENC28J60_BANK_2             (ENC28J60_BANKED_MASK | 0x80)
 #define ENC28J60_BANK_3             (ENC28J60_BANKED_MASK | 0xC0)
+#define ENC28J60_ADDR_ETXSTL        (ENC28J60_BANK_0 | 0x04)
+#define ENC28J60_ADDR_ETXSTH        (ENC28J60_BANK_0 | 0x05)
+#define ENC28J60_ADDR_ETXNDL        (ENC28J60_BANK_0 | 0x06)
+#define ENC28J60_ADDR_ETXNDH        (ENC28J60_BANK_0 | 0x07)
+#define ENC28J60_ADDR_ERXSTL        (ENC28J60_BANK_0 | 0x08)
+#define ENC28J60_ADDR_ERXSTH        (ENC28J60_BANK_0 | 0x09)
+#define ENC28J60_ADDR_ERXNDL        (ENC28J60_BANK_0 | 0x0A)
+#define ENC28J60_ADDR_ERXNDH        (ENC28J60_BANK_0 | 0x0B)
+#define ENC28J60_ADDR_ERXRDPTL      (ENC28J60_BANK_0 | 0x0C)
+#define ENC28J60_ADDR_ERXRDPTH      (ENC28J60_BANK_0 | 0x0D)
 #define ENC28J60_ADDR_ERXFCON       (ENC28J60_BANK_1 | 0x18)
+#define ENC28J60_ADDR_MACON1        (ENC28J60_BANK_2 | 0x00)
 #define ENC28J60_ADDR_MACON3        (ENC28J60_BANK_2 | 0x02)
 #define ENC28J60_ADDR_MABBIPG       (ENC28J60_BANK_2 | 0x04)
 #define ENC28J60_ADDR_MAIPGL        (ENC28J60_BANK_2 | 0x06)
+#define ENC28J60_ADDR_MAIPGH        (ENC28J60_BANK_2 | 0x07)
 #define ENC28J60_ADDR_MAMXFLL       (ENC28J60_BANK_2 | 0x0A)
 #define ENC28J60_ADDR_MAMXFLH       (ENC28J60_BANK_2 | 0x0B)
+#define ENC28J60_ADDR_MICMD         (ENC28J60_BANK_2 | 0x12)
 #define ENC28J60_ADDR_MIREGADR      (ENC28J60_BANK_2 | 0x14)
 #define ENC28J60_ADDR_MIWRL         (ENC28J60_BANK_2 | 0x16)
 #define ENC28J60_ADDR_MIWRH         (ENC28J60_BANK_2 | 0x17)
-#define ENC28J60_ADDR_MAIPGH        (ENC28J60_BANK_2 | 0x07)
+#define ENC28J60_ADDR_MIRDL         (ENC28J60_BANK_2 | 0x18)
+#define ENC28J60_ADDR_MIRDH         (ENC28J60_BANK_2 | 0x19)
 #define ENC28J60_ADDR_MISTAT        (ENC28J60_BANK_3 | 0x0A)
 #define ENC28J60_ADDR_EREVID        (ENC28J60_BANK_3 | 0x12)
+
+/* ENC28J60 non-banked control registers. */
+#define ENC28J60_ADDR_EIE           (0x1B)
+#define ENC28J60_ADDR_EIR           (0x1C)
+#define ENC28J60_ADDR_ECON2         (0x1E)
+#define ENC28J60_ADDR_ECON1         (0x1F)
+#define ENC28J60_ADDR_RESET         (0x1F)
 
 /* ENC28J60 PHY registers. */
 #define ENC28J60_ADDR_PHCON1        (0x00)
 #define ENC28J60_ADDR_PHSTAT1       (0x01)
 #define ENC28J60_ADDR_PHCON2        (0x10)
-
-/* ENC28J60 control registers. */
-#define ENC28J60_ADDR_ECON2         (0x1E)
-#define ENC28J60_ADDR_ECON1         (0x1F)
-#define ENC28J60_ADDR_RESET         (0x1F)
-
-/* ENC28J60 ECON2 register definitions. */
-#define ENC28J60_ECON2_AUTOINC      (0x80)
-#define ENC28J60_ECON2_PKTDEC       (0x40)
-#define ENC28J60_ECON2_PWRSV        (0x20)
-#define ENC28J60_ECON2_VRPS         (0x08)
+#define ENC28J60_ADDR_PHIE          (0x12)
+#define ENC28J60_ADDR_PHIR          (0x13)
 
 /* ENC28J60 ERXFCON register definitions. */
 #define ENC28J60_ERXFCON_UCEN       (0x80)
@@ -69,6 +81,13 @@
 #define ENC28J60_ERXFCON_MCEN       (0x02)
 #define ENC28J60_ERXFCON_BCEN       (0x01)
 
+/* ENC28J60 MACON1 register definitions */
+#define ENC28J60_MACON1_LOOPBK      (0x10)
+#define ENC28J60_MACON1_TXPAUS      (0x08)
+#define ENC28J60_MACON1_RXPAUS      (0x04)
+#define ENC28J60_MACON1_PASSALL     (0x02)
+#define ENC28J60_MACON1_MARXEN      (0x01)
+
 /* ENC28J60 MACON3 register definitions. */
 #define ENC28J60_MACON3_PADCFG2     (0x80)
 #define ENC28J60_MACON3_PADCFG1     (0x40)
@@ -79,12 +98,41 @@
 #define ENC28J60_MACON3_FRMLNEN     (0x02)
 #define ENC28J60_MACON3_FULDPX      (0x01)
 
+/* ENC28J60 MICMD register definitions. */
+#define ENC28J60_MICMD_MIISCAN      (0x02)
+#define ENC28J60_MICMD_MIIRD        (0x01)
+
 /* ENC28J60 MISTAT register definitions. */
 #define ENC28J60_MISTAT_NVALID      (0x04)
 #define ENC28J60_MISTAT_SCAN        (0x02)
 #define ENC28J60_MISTAT_BUSY        (0x01)
 
-/* ENC28J60 ECON register definitions. */
+/* ENC28J60 EIE register definitions. */
+#define ENC28J60_EIE_INTIE          (0x80)
+#define ENC28J60_EIE_PKTIE          (0x40)
+#define ENC28J60_EIE_DMAIE          (0x20)
+#define ENC28J60_EIE_LINKIE         (0x10)
+#define ENC28J60_EIE_TXIE           (0x08)
+#define ENC28J60_EIE_WOLIE          (0x04)
+#define ENC28J60_EIE_TXERIE         (0x02)
+#define ENC28J60_EIE_RXERIE         (0x01)
+
+/* ENC28J60 EIR register definitions. */
+#define ENC28J60_EIR_PKTIF          (0x40)
+#define ENC28J60_EIR_DMAIF          (0x20)
+#define ENC28J60_EIR_LINKIF         (0x10)
+#define ENC28J60_EIR_TXIF           (0x08)
+#define ENC28J60_EIR_WOLIF          (0x04)
+#define ENC28J60_EIR_TXERIF         (0x02)
+#define ENC28J60_EIR_RXERIF         (0x01)
+
+/* ENC28J60 ECON2 register definitions. */
+#define ENC28J60_ECON2_AUTOINC      (0x80)
+#define ENC28J60_ECON2_PKTDEC       (0x40)
+#define ENC28J60_ECON2_PWRSV        (0x20)
+#define ENC28J60_ECON2_VRPS         (0x08)
+
+/* ENC28J60 ECON1 register definitions. */
 #define ENC28J60_ECON1_TXRST        (0x80)
 #define ENC28J60_ECON1_RXRST        (0x40)
 #define ENC28J60_ECON1_DMAST        (0x20)
@@ -100,6 +148,14 @@
 #define ENC28J60_PHCON1_PPWRSV      (0x0800)
 #define ENC28J60_PHCON1_PDPXMD      (0x0100)
 
+/* ENC28J60 PHIE register definitions. */
+#define ENC28J60_PHIE_PLNKIE        (0x10)
+#define ENC28J60_PHIE_PGEIE         (0x02)
+
+/* ENC28J60 PHIR register definitions. */
+#define ENC28J60_PHIR_PLNKIF        (0x10)
+#define ENC28J60_PHIR_PGEIF         (0x02)
+
 /* ENC28J60 common value definitions. */
 #define ENC28J60_VALUE_RESET        (0xFF)
 
@@ -111,6 +167,9 @@
 
 /* Function prototypes. */
 int32_t enc28j60_write_phy(ENC28J60 *, uint8_t, uint16_t);
+int32_t enc28j60_read_phy(ENC28J60 *, uint8_t, uint16_t *);
+int32_t enc28j60_write_word(ENC28J60 *, uint8_t, uint16_t);
+int32_t enc28j60_read_word(ENC28J60 *, uint8_t, uint16_t *);
 int32_t enc28j60_write_read_op(ENC28J60 *, uint8_t, uint8_t, uint8_t, uint8_t *);
 
 #endif /* CONFIG_ENC28J60 */
