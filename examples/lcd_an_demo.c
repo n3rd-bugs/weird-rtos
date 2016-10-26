5ba4d9a7368c0ae60b98518c4c2a0ec3f158a849
N3rd Bugs <mirzaon@gmail.com>
2016-10-26 21:48:55 +0500

    Updated LCD demo.

--- examples/lcd_an_demo.c
+++ examples/lcd_an_demo.c
@@ -17,10 +17,10 @@
 #include <fs.h>
 #include <sys_info.h>
 #include <math.h>
-#include <lcd.h>
+#include <lcd_an.h>
 
 /* LCD Demo task definitions. */
-#define LCD_DEMO_TASK_STACK_SIZE        512
+#define LCD_DEMO_TASK_STACK_SIZE        384
 uint8_t lcd_demo_stack[LCD_DEMO_TASK_STACK_SIZE];
 TASK    lcd_demo_cb;
 void lcd_demo_entry(void *argv);
@@ -33,9 +33,31 @@ void lcd_demo_entry(void *argv);
 void lcd_demo_entry(void *argv)
 {
     uint64_t systick;
+#ifdef LCD_AN_DEBUG
+    extern FD lcd_an_fd;
+    LCD_AN_IOCTL_DATA ioctl_data;
+#endif
 
-    /* Initialize LCD. */
-    lcd_init();
+    /* Initialize alphanumeric LCD. */
+    lcd_an_init();
+
+    /* Create custom characters. */
+    ioctl_data.index = 0;
+    ioctl_data.param = (uint8_t []){ 0b01010, 0b10101, 0b10001, 0b01010,
+                                     0b00100, 0b01010, 0b01110, 0b01010 };
+    fs_ioctl(lcd_an_fd, LCD_AN_CUSTOM_CHAR, &ioctl_data);
+    ioctl_data.index = 1;
+    ioctl_data.param = (uint8_t []){ 0b01100, 0b01010, 0b01100, 0b01010,
+                                     0b01000, 0b01100, 0b01010, 0b01010 };
+    fs_ioctl(lcd_an_fd, LCD_AN_CUSTOM_CHAR, &ioctl_data);
+    ioctl_data.index = 2;
+    ioctl_data.param = (uint8_t []){ 0b00100, 0b01010, 0b01110, 0b01010,
+                                     0b00000, 0b01010, 0b01110, 0b01010 };
+    fs_ioctl(lcd_an_fd, LCD_AN_CUSTOM_CHAR, &ioctl_data);
+    ioctl_data.index = 3;
+    ioctl_data.param = (uint8_t []){ 0b01010, 0b01010, 0b00100, 0b01010,
+                                     0b10101, 0b10001, 0b01010, 0b00100 };
+    fs_ioctl(lcd_an_fd, LCD_AN_CUSTOM_CHAR, &ioctl_data);
 
     /* Move to next 500ms boundary. */
     sleep_ms(500 - (TICK_TO_MS(current_system_tick()) % 500));
@@ -49,9 +71,9 @@ void lcd_demo_entry(void *argv)
         systick = current_system_tick();
 
         /* Print "Hello World" with current system tick. */
-        printf("\f\1\tWeird RTOS\r\n\tLCD Demo\r\n");
-        printf("SysTick\t: %lu", (uint32_t)systick);
-        printf("\r\nStack\t\t: %lu", get_current_task()->stack_size - (uint32_t)util_task_calc_free_stack(get_current_task()));
+        printf("\f\10\tWeird RTOS\r\n\1\tLCD Demo\r\n");
+        printf("\2SysTick\t: %lu", (uint32_t)systick);
+        printf("\r\n\3Stack\t: %lu", get_current_task()->stack_size - (uint32_t)util_task_calc_free_stack(get_current_task()));
 
         /* Sleep for 500ms. */
         sleep_ms(500 - TICK_TO_MS(current_system_tick() - systick));
