4425d25f574486ef0c15ad78e7d7022bc3204e96
Masood <umasood@mgc.mentorg.com>
2017-07-03 10:42:59 +0500

    Moved initializetion in init.c for AVR.

--- port/kernel/avr/avr.c
+++ port/kernel/avr/avr.c
@@ -15,7 +15,6 @@
 #include <avr/wdt.h>
 #include <kernel.h>
 #include <avr.h>
-#include <string.h>
 
 /* Global interrupt level. */
 /* TRUE: Interrupt Enabled
@@ -172,63 +171,6 @@ void stack_init(TASK *tcb, TASK_ENTRY *entry, void *argv)
     (tcb->tos)--;
 
 } /* stack_init */
-
-/*
- * avr_stack_fill
- * This function will fill the stack with predefined pattern.
- */
-void avr_stack_fill(void) __attribute__((naked)) __attribute__((section(".init1")));
-void avr_stack_fill(void)
-{
-#ifdef CONFIG_TASK_STATS
-    uint8_t *stack = &__heap_start;
-
-    /* Load a predefined pattern on the system stack until we hit the
-     * stack pointer. */
-    while ((uint8_t *)SP > stack)
-    {
-        /* Load a predefined pattern. */
-        *(stack++) = CONFIG_STACK_PATTERN;
-    }
-#endif /* CONFIG_TASK_STATS */
-} /* avr_stack_fill */
-
-/*
- * avr_stack_init
- * This function will disable WDT and perform boatload operation if required.
- */
-void avr_stack_init(void) __attribute__((naked)) __attribute__((section(".init3")));
-void avr_stack_init(void)
-{
-    /* Disable watch dog timer. */
-    MCUSR = 0;
-    wdt_disable();
-
-#ifdef CONFIG_BOOTLOAD
-    /* Initialize boot loader condition. */
-    BOOTLOAD_COND_INIT;
-
-    /* If boot condition meets. */
-    if (BOOTLOAD_COND)
-    {
-        /* Perform boot load operation. */
-        BOOTLOAD();
-    }
-#endif
-
-} /* avr_stack_init */
-
-/*
- * avr_sys_stack_pointer_save
- * This function will save the system stack pointer to be used when needed.
- */
-void avr_sys_stack_pointer_save(void) __attribute__((naked)) __attribute__((section(".init8")));
-void avr_sys_stack_pointer_save(void)
-{
-    /* Save the system stack pointer. */
-    system_stack_end = (uint8_t *)SP;
-
-} /* avr_sys_stack_pointer_save */
 
 /*
  * control_to_system
---
+++ port/target/atmega644p/board/init.c
@@ -0,0 +1,73 @@
+/*
+ * init.c
+ *
+ * Copyright (c) 2017 Usama Masood <mirzaon@gmail.com>
+ *
+ * Standard MIT License apply on this source code, with the inclusion of below
+ * clause.
+ *
+ * This source is for educational purpose only, and should never be used for
+ * any other purpose. If this source is used for other than educational purpose
+ * (in any form) the author will not be liable for any outcome from its direct
+ * or indirect use.
+ */
+#include <kernel.h>
+#include <avr/wdt.h>
+#include <avr.h>
+
+/*
+ * avr_stack_fill
+ * This function will fill the stack with predefined pattern.
+ */
+void avr_stack_fill(void) __attribute__((naked)) __attribute__((section(".init1")));
+void avr_stack_fill(void)
+{
+#ifdef CONFIG_TASK_STATS
+    uint8_t *stack = &__heap_start;
+
+    /* Load a predefined pattern on the system stack until we hit the
+     * stack pointer. */
+    while ((uint8_t *)SP > stack)
+    {
+        /* Load a predefined pattern. */
+        *(stack++) = CONFIG_STACK_PATTERN;
+    }
+#endif /* CONFIG_TASK_STATS */
+} /* avr_stack_fill */
+
+/*
+ * avr_stack_init
+ * This function will disable WDT and perform boatload operation if required.
+ */
+void avr_stack_init(void) __attribute__((naked)) __attribute__((section(".init3")));
+void avr_stack_init(void)
+{
+    /* Disable watch dog timer. */
+    MCUSR = 0;
+    wdt_disable();
+
+#ifdef CONFIG_BOOTLOAD
+    /* Initialize boot loader condition. */
+    BOOTLOAD_COND_INIT;
+
+    /* If boot condition meets. */
+    if (BOOTLOAD_COND)
+    {
+        /* Perform boot load operation. */
+        BOOTLOAD();
+    }
+#endif
+
+} /* avr_stack_init */
+
+/*
+ * avr_sys_stack_pointer_save
+ * This function will save the system stack pointer to be used when needed.
+ */
+void avr_sys_stack_pointer_save(void) __attribute__((naked)) __attribute__((section(".init8")));
+void avr_sys_stack_pointer_save(void)
+{
+    /* Save the system stack pointer. */
+    system_stack_end = (uint8_t *)SP;
+
+} /* avr_sys_stack_pointer_save */
