4f762949f4366a39165a4cb39b47f6a31da61e37
Masood <umasood@mgc.mentorg.com>
2017-08-07 17:00:28 +0500

    Updated configuration with recent updates.

--- rtos/kernel/assert.cmake
+++ rtos/kernel/assert.cmake
@@ -1,3 +1,3 @@
 # Setup assert configuration options.
-setup_option_def(ASSERT_NONE ON BOOL "Disable assert conditions.")
-setup_option_def(ASSERT_FILE_INFO ON BOOL "Disable file information from assert.")
+setup_option_def(ASSERT_NONE ON DEFINE "Disable assert conditions.")
+setup_option_def(ASSERT_FILE_INFO ON DEFINE "Disable file information from assert.")
--- rtos/kernel/tasks.cmake
+++ rtos/kernel/tasks.cmake
@@ -1,4 +1,4 @@
 # Setup tasks configuration options.
-setup_option_def(CONFIG_TASK_STATS OFF BOOL "Enable tasks statistics including task stack usage.")
-setup_option_def(CONFIG_TASK_USAGE OFF BOOL "Enable CPU usage statistics for tasks.")
+setup_option_def(CONFIG_TASK_STATS OFF DEFINE "Enable tasks statistics including task stack usage.")
+setup_option_def(CONFIG_TASK_USAGE OFF DEFINE "Enable CPU usage statistics for tasks.")
 setup_option_def(CONFIG_STACK_PATTERN A CHAR "Pattern character to be used to fill a task stack.")
