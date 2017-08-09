cac8d733556714b08c06441b60c8ef10377b14c1
Masood <umasood@mgc.mentorg.com>
2017-08-09 16:24:21 +0500

    Updated cmake helper.

--- cmake/modules/helper.cmake
+++ cmake/modules/helper.cmake
@@ -1,15 +1,3 @@
-# This function will configure an option.
-function (setup_option configuration value)
-    # Set this option as provided.
-    set(${configuration} ${value} CACHE INTERNAL "" FORCE)
-endfunction ()
-
-# This function will make an option hidden.
-function (setup_option_hide configuration)
-    # Force to to hide this option.
-    set(${configuration} ${${configuration}} CACHE INTERNAL "" FORCE)
-endfunction ()
-
 # This function will setup an RTOS configuration.
 function (setup_option_def configuration default_value type description)
     # Pick configuration type.
@@ -32,34 +20,71 @@ function (setup_option_def configuration default_value type description)
         # If option is enabled.
         if (${configuration})
             # Add this to RTOS configuration list.
-            SET(RTOS_DEFS "${RTOS_DEFS}#define ${configuration}\n" CACHE INTERNAL "RTOS_DEFS" FORCE)
+            set(RTOS_DEFS "${RTOS_DEFS}#define ${configuration}\n" CACHE INTERNAL "RTOS_DEFS" FORCE)
         endif ()
     # If this is a boolean option.
     elseif (${type} STREQUAL "BOOL")
         # If option is enabled.
         if (${configuration})
             # Add this to RTOS configuration list as TRUE.
-            SET(RTOS_DEFS "${RTOS_DEFS}#define ${configuration} TRUE\n" CACHE INTERNAL "RTOS_DEFS" FORCE)
+            set(RTOS_DEFS "${RTOS_DEFS}#define ${configuration} TRUE\n" CACHE INTERNAL "RTOS_DEFS" FORCE)
         else ()
             # Add this to RTOS configuration list as FALSE.
-            SET(RTOS_DEFS "${RTOS_DEFS}#define ${configuration} FALSE\n" CACHE INTERNAL "RTOS_DEFS" FORCE)
+            set(RTOS_DEFS "${RTOS_DEFS}#define ${configuration} FALSE\n" CACHE INTERNAL "RTOS_DEFS" FORCE)
         endif ()
     elseif (${type} STREQUAL "STRING")
         # Add this to RTOS configuration list with a string value.
-        SET(RTOS_DEFS "${RTOS_DEFS}#define ${configuration} \"${${configuration}}\"\n" CACHE INTERNAL "RTOS_DEFS" FORCE)
+        set(RTOS_DEFS "${RTOS_DEFS}#define ${configuration} \"${${configuration}}\"\n" CACHE INTERNAL "RTOS_DEFS" FORCE)
     elseif (${type} STREQUAL "CHAR")
         # Add this to RTOS configuration list with a character value.
-        SET(RTOS_DEFS "${RTOS_DEFS}#define ${configuration} \'${${configuration}}\'\n" CACHE INTERNAL "RTOS_DEFS" FORCE)
+        set(RTOS_DEFS "${RTOS_DEFS}#define ${configuration} \'${${configuration}}\'\n" CACHE INTERNAL "RTOS_DEFS" FORCE)
     elseif (${type} STREQUAL "INT")
         # Add this to RTOS configuration list with an integral value.
-        SET(RTOS_DEFS "${RTOS_DEFS}#define ${configuration} \(${${configuration}}\)\n" CACHE INTERNAL "RTOS_DEFS" FORCE)
+        set(RTOS_DEFS "${RTOS_DEFS}#define ${configuration} \(${${configuration}}\)\n" CACHE INTERNAL "RTOS_DEFS" FORCE)
     elseif (${type} STREQUAL "MACRO")
         # Add this to RTOS configuration list as a MACRO value.
-        SET(RTOS_DEFS "${RTOS_DEFS}#define ${configuration} ${${configuration}}\n" CACHE INTERNAL "RTOS_DEFS" FORCE)
+        set(RTOS_DEFS "${RTOS_DEFS}#define ${configuration} \(${${configuration}}\)\n" CACHE INTERNAL "RTOS_DEFS" FORCE)
     else ()
         # Option type is not supported.
         message(FATAL_ERROR "Unsupported option type." )
     endif ()
+endfunction ()
+
+# This function will configure an option.
+function (setup_option configuration value)
+    # Set this option as provided.
+    set(${configuration} ${value} CACHE INTERNAL "" FORCE)
+endfunction ()
+
+# This function will make an option hidden.
+function (setup_option_hide configuration)
+    # Force to to hide this option.
+    set(${configuration} ${${configuration}} CACHE INTERNAL "" FORCE)
+endfunction ()
+
+# This function will set valid options for a configuration.
+function (setup_option_values configuration values)
+    # Set possible options for this configuration.
+    set_property(CACHE ${configuration} PROPERTY STRINGS ${${values}})
+    if(NOT ${${configuration}} IN_LIST ${values})
+        message(FATAL_ERROR "${configuration} must be one of ${${values}}")
+    endif()
+endfunction ()
+
+# This function will test if all the members in the given configuration do exist in the given list.
+function (setup_option_list_values configuration values delimiter)
+    # First remove all the spaces.
+    string(REPLACE " " "" configuration_list ${${configuration}})
+    
+    # Now divide the string on the given delimiter.
+    string(REPLACE ${delimiter} ";" configuration_list ${configuration_list})
+    
+    # Verify all the items in the configuration does exist in the given list.
+    foreach(list_item ${configuration_list})
+        if(NOT ${list_item} IN_LIST ${values})
+            message(FATAL_ERROR "${configuration} must only contain these ${${values}}")
+        endif()
+    endforeach()
 endfunction ()
 
 # This function will generate configuration file.
