064fa846663c395f5af29c16b0d968bde97d0db7
N3rd Bugs <mirzaon@gmail.com>
2017-08-13 14:48:27 +0500

    Revert "Updated smart change over project for 16MHz."
    This reverts commit b4757ce32d933f22177a2443aec9a9275bf533ff.

--- examples/smart_change_over/smart_change_over.c
+++ examples/smart_change_over/smart_change_over.c
@@ -22,13 +22,8 @@
 #include <weird_view_server.h>
 #include <adc.h>
 #include <math.h>
-#include <avr/wdt.h>
-#ifdef CONFIG_LCD_AN
 #include <lcd_an.h>
-#endif /* CONFIG_LCD_AN */
-#ifdef CONFIG_SERIAL
-#include <serial.h>
-#endif
+#include <avr/wdt.h>
 
 /* Definitions to communicate with other side. */
 #define DEVICE_NAME         "Smart Change Over"
@@ -94,7 +89,7 @@ WEIRD_VIEW_PLUGIN           weird_view_plugins[] =
 };
 
 /* Control task definitions. */
-#define CONTROL_TASK_STACK_SIZE         192
+#define CONTROL_TASK_STACK_SIZE         160
 uint8_t control_stack[CONTROL_TASK_STACK_SIZE];
 TASK control_cb;
 void control_entry(void *argv);
@@ -106,9 +101,9 @@ TASK log_cb;
 void log_entry(void *argv);
 
 /* ADC configuration and data. */
-#define ADC_PRESCALE            ((uint32_t)25)
+#define ADC_PRESCALE            ((uint32_t)61)
 #define ADC_WAVE_FREQ           ((uint32_t)100)
-#define ADC_ATIMER_PRESCALE     ((uint32_t)256)
+#define ADC_ATIMER_PRESCALE     ((uint32_t)64)
 #define ADC_SAMPLE_PER_WAVE     ((uint32_t)PCLK_FREQ / (ADC_ATIMER_PRESCALE * ADC_PRESCALE * ADC_WAVE_FREQ))
 
 /* Charge controller definitions. */
