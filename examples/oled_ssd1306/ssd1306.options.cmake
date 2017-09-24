2c48b7a1fe7e360e8a0a840f422b7f093bbb656c
N3rd Bugs <mirzaon@gmail.com>
2017-09-24 18:10:09 +0500

    Updated OLED demo configuration.

--- examples/oled_ssd1306/ssd1306.options.cmake
+++ examples/oled_ssd1306/ssd1306.options.cmake
@@ -1,6 +1,10 @@
 # Include helpers.
 include(${RTOS_ROOT}/cmake/modules/helper.cmake)
 
+# Setup driver configurations.
+setup_option(CONFIG_I2C ON)
+setup_option(CONFIG_OLED ON)
+
 # Setup task options.
 setup_option(CONFIG_TASK_STATS ON)
 setup_option(CONFIG_TASK_USAGE ON)
