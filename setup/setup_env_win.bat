093fa7e11454727b21d2eef67ed2fabc3e196d09
Usama Masood <mirzaon@gmail.com>
2019-12-24 17:28:52 +0500

    Updated setup_env_win with new paths.

--- setup/setup_env_win.bat
+++ setup/setup_env_win.bat
@@ -5,17 +5,19 @@
 
 :: Populate paths.
 set ARM_TOOLSET=C:\Program Files (x86)\GNU Tools Arm Embedded\7 2018-q2-update\bin\
-set AVR_TOOLSET=D:\Hobby\avr8-gnu-toolchain\bin\
+set AVR_TOOLSET=C:\private\avr8-gnu-toolchain\bin\
+set OPENOCD=C:\private\openocd-20190210\OpenOCD-20190210-0.10.0\bin
 set PYTHON_PATH=D:\Hobby\python\
 set PYTHON_SCRIPTS_PATH=D:\Hobby\python\Scripts
+set MAKE_PATH=C:\private\GnuWin32\bin
 set CMAKE_PATH="C:\Program Files\CMake\bin\"
-set ECLIPSE_PATH="D:\Hobby\eclipse\"
+set ECLIPSE_PATH="C:\private\eclipse"
 
 :: Add ARM toolchain.
-setx path "%AVR_TOOLSET%;%ARM_TOOLSET%;%PYTHON_PATH%;%PYTHON_SCRIPTS_PATH%;%%path%%" >nul 2>&1
+set PATH=%AVR_TOOLSET%;%ARM_TOOLSET%;%OPENOCD%;%MAKE_PATH%;%PYTHON_PATH%;%PYTHON_SCRIPTS_PATH%;%PATH%
 
 :: Execute CMAKE.
-start "" %CMAKE_PATH%\cmake-gui.exe -D"CMAKE_MAKE_PROGRAM:PATH=D:\Hobby\avr8-gnu-toolchain\bin"
+start "" %CMAKE_PATH%\cmake-gui.exe
 start "" %ECLIPSE_PATH%\eclipse.exe
 
 :: exit
